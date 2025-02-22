aa64f10214c488e84050a0741671a065dc557e37
Yogurt:) <153968747+Jealous7@users.noreply.github.com>
2025-02-22 12:23:22 +0800

    push

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const methodOverride = require('method-override');
 const path = require('path');
 const PORT = 3000;
 
+//connect to database
+mongoose.connect('mongodb://127.0.0.1:27017/therapy')
+    .then(() => console.log('Connection successful'))
+    .catch(e => console.log(e));
+mongoose.set('strictQuery', true);
+
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
 
 app.listen(PORT, () => {
     console.log('Serving on port 3000')
