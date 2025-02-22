134ff7a967d874d950936f7b56a2fcc8792efac8
fantasticdj <jung112629@gapps.uwcsea.edu.sg>
2025-02-22 16:04:30 +0800

    sdf

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+app.get('/', (req, res)=>{
+    res.render('index')
+})
 
 app.listen(PORT, () => {
     console.log('Serving on port 3000')
