5630c4a3df0cb2fabd810aa0b729a3983b54893a
Yogurt:) <153968747+Jealous7@users.noreply.github.com>
2025-02-22 11:20:15 +0800

    push

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const path = require('path');
 const PORT = 3000;
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.listen(PORT, () => {
+    console.log('Serving on port 3000')
+})
